9edda7a6e47fa1e83f211ac088a7bdb498cbc66a
Areeta Wong <areetaw@gmail.com>
2017-10-27 17:33:34 -0700

    Test Project

---
+++ MusicPlayer/app/src/main/java/com/example/areetaw/musicplayer/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.areetaw.musicplayer;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.moana);
+
+
+        Button play = (Button) findViewById(R.id.play_button);
+        Button pause = (Button) findViewById(R.id.pause_button);
+        Button reset = (Button) findViewById(R.id.reset_button);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+         });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.reset();
+            }
+        });
+    }
+}
+
