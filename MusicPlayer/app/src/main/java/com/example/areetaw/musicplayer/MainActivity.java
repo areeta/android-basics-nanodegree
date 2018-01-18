6bd52cbe8b98561422be4bb45348f03c586317ae
Areeta Wong <areetaw@gmail.com>
2018-01-18 01:42:34 -0800

    toast

--- MusicPlayer/app/src/main/java/com/example/areetaw/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/example/areetaw/musicplayer/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        Toast.makeText(MainActivity.this,"I'm done!",Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
          });
 
