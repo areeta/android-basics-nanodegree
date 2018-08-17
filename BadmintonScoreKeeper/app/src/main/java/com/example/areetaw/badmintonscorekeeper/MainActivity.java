2b612a3e532c89dd63929de8798591a6f2ab4c60
Areeta Wong <areetaw@gmail.com>
2018-08-17 16:22:10 -0700

    Badminton Scorer

--- BadmintonScoreKeeper/app/src/main/java/com/example/areetaw/badmintonscorekeeper/MainActivity.java
+++ BadmintonScoreKeeper/app/src/main/java/com/example/areetaw/badmintonscorekeeper/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.areetaw.badmintonscorekeeper;
 
-        import android.os.Bundle;
-        import android.support.v7.app.AppCompatActivity;
-        import android.view.View;
-        import android.widget.TextView;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
